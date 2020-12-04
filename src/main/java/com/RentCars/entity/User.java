a1bab76aea354be9307d92e9b6958bed30b0d0bb
Alexander <tsybinsasha5@gmail.com>
2020-12-04 18:03:27 +0300

    Create new project RentCars  with //start.spring.io add gitignore add pox.xml add application.properties add index.html add DAO User

---
+++ src/main/java/com/RentCars/entity/User.java
@@ -0,0 +1,34 @@
+package com.RentCars.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "m_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String surname;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date birth_date;
+
+    @Column
+    private String gender;
+
+    @Column
+    private String passport;
+}
